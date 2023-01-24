ffb7381579ca6f4e5c1f7456da50bee0d52d7c43
Coltrane Willsey <cwillsey06@gmail.com>
2023-01-23 22:43:17 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,49 @@
 #!/bin/sh
 
-sh_dirname() {
-    dir=${1:-.}
-    dir=${dir%%"${dir##*[!/]}"}
-    [ "${dir##*/*}" ] && dir=.
-    dir=${dir%/*}
-    dir=${dir%%"${dir##*[!/]}"}
-    printf '%s\n' "${dir:-/}"
+SRC_DIR=$(pwd)
+
+clean() {
+	for DIR in */ ; do
+		cd $DIR
+		printf '%s\n' "removed ${DIR}src"
+		rm -rf src/
+		cd $SRC_DIR
+	done
 }
 
-for DIR in */ ; do
-	cd $DIR
-	git clone $(cat src.txt)
-	make clean build
-done
+build() {
+	for DIR in */ ; do
+		cd $DIR
+		printf '\n%s\n' "-> build: $DIR"
+	
+		git clone $(cat src.txt) src/
+	
+		for DIFF in *.diff ; do
+			cd src
+			if [ -f "$file" ]; then
+				patch -p1 < $DIFF
+			fi
+			make 
+			cd ..
+		done
+	
+		cd $SRC_DIR
+	done
+}
+
+case $1 in
+	"-h")
+		printf '%s\n'	"Usage: build.sh [-c|-h]" 
+		printf '%s\n'	"With no arguments, clean & perform build task"
+		printf '%s\n'	"  -c clean directories"
+		printf '%s\n\n'	"  -h display help information";;
+	"-c")
+		printf '%s\n' "==> cleaning.."
+		clean;;
+	*)
+		printf '%s\n' "==> cleaning.."
+		clean
+		printf '\n%s' "==> building.."
+		build;;
+esac
 
